04471d124240d39ba965ef3150214080ac6213e2
halbersc <halbersc@gmail.com>
2020-01-21 16:49:49 -0500

    actualización de los componentes people and films

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeopleComponent } from './people/people.component';
+import { FilmsComponent } from './films/films.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeopleComponent,
+    FilmsComponent
   ],
   imports: [
     BrowserModule,
